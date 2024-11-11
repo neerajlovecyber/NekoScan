3084ce25dabbe971d55c377c18915c484616260d
neerajlovecyber <neerajlovecyber@gmail.com>
2024-11-11 22:25:16 +0530

    minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import { AppSidebar } from "@/components/app-sidebar";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
 import { SidebarInset, SidebarProvider,SidebarTrigger } from "@/components/ui/sidebar";
-
-import { ActiveScans } from "./components/RunningScans";
-
 import { ModeToggle } from "./components/mode-toggle";
 import { Settings } from "@/components/Settings";
 import { Profiles } from "@/components/Profiles";
